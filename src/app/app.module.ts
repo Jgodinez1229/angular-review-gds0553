2f8624e72a1a6de333369edfa7cbbc7a3f5c0102
Josué Godínez <nunezomar294@gmail.com>
2024-02-07 14:31:52 -0600

    Subiendo proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ButtonModule } from 'primeng/button';
+
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PrimengModule } from './primeng.module';
+import { LoginComponent } from './componentes/login/login.component';
+import { RegisterComponent } from './componentes/register/register.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ButtonModule 
+    ReactiveFormsModule,
+    HttpClientModule,
+    PrimengModule,
+    BrowserAnimationsModule
+
   ],
   providers: [
     provideClientHydration()
